bc1fd1192afbb43f7156274353dcfea96ddae039
Zhou Hua <zhou--hua@163.com>
2024-07-13 21:33:44 +0800

    feat: calc indent level

--- src/utils.ts
+++ src/utils.ts
@@ -46,3 +46,40 @@ export function trivial(
     mode,
   );
 }
+
+function findLastFromindex<T = unknown>(list: T[], lastIndex: number, check: (item: T) => boolean): number {
+  let index = -1;
+  for (let i = lastIndex; i >= 0; i--) {
+    if (check(list[i])) {
+      index = i;
+      break;
+    }
+  }
+  return index;
+}
+
+export function calcIndentLevels(headings: HeadingCache[]): number[] {
+  const result: number[] = [];
+  if (!headings.length) {
+    return result;
+  }
+  const topLevelIndex = headings.reduce<number>(
+    (res, cur, i) => (cur.level < headings[res].level ? i : res),
+    0,
+  );
+  result.push(...calcIndentLevels(headings.slice(0, topLevelIndex)).map(level => level + 1));
+  headings.slice(topLevelIndex).forEach((heading, i, list) => {
+    if (i === 0) {
+      result.push(1);
+      return;
+    }
+    const parentIndex = findLastFromindex(list, i - 1, item => item.level < heading.level);
+    if (parentIndex === -1) {
+      result.push(0);
+    }
+    else {
+      result.push(result[parentIndex + topLevelIndex] + 1);
+    }
+  });
+  return result;
+}
