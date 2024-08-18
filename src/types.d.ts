a6b9c10516eab2e6e9ba693f0acef8441b035471
itsonlyjames <jamesalexandre1995@gmail.com>
2024-08-19 07:45:56 +1000

    Re-add necessary type extensions

--- src/types.d.ts
+++ src/types.d.ts
@@ -24,8 +24,27 @@ export interface Heading extends HeadingCache {
 }
 
 declare module 'obsidian' {
+  interface WorkspaceLeaf {
+    id: string;
+  }
+  interface MarkdownView {
+    getFile: () => TFile;
+    editMode: {
+      sourceMode: boolean;
+      containerEl: {
+        querySelector: (string) => HTMLElement;
+      };
+    };
+  }
+
   interface MarkdownSubView {
     type: 'source' | 'preview';
+  }
+  interface Editor {
+    cm: {
+      scrollDOM: HTMLElement;
+      lineBlockAt: (Pos) => {};
+    };
   }
   interface MarkdownPreviewView {
     renderer: {
